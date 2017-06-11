d6777035d495db8996f9d876df4d6c35fec63d81
Daniel Fernandez <dafasion@Daniels-MacBook-Pro-Ret-2.local>
2017-06-11 15:56:37 -0500

    Save my work

---
+++ js/script.js
@@ -0,0 +1,205 @@
+// Write JavaScript here
+
+function bg1(){
+  var num = getRandomInt(0, 36);
+  switch (num) {
+    case 0:
+    document.body.style.backgroundColor = "#f3f3f3";
+    document.body.style.backgroundImage = "url('assets/2-0.0-0.0-0.0-58-0-4-21-2-24-0-3.png')";
+      break;
+      case 1:
+      document.body.style.backgroundColor = "#f3f3f3";
+      document.body.style.backgroundImage = "url('assets/2-0.0-0.0-0.0-73-0-3-14-24-4-0-4.png')";
+      break;
+      case 2:
+      document.body.style.backgroundColor = "#f3f3f3";        document.body.style.backgroundImage = "url('assets/2-0.0-0.0-0.0-112-0-0-2-25-38-23-6-15.png')";
+      break;
+      case 3:
+      document.body.style.backgroundColor = "#f3f3f3";        document.body.style.backgroundImage = "url('assets/3-0.0-0.45-1.0-83-0-0-2-27-46-5-4-2.png')";
+      break;
+      case 4:
+      document.body.style.backgroundColor = "#f3f3f3";            document.body.style.backgroundImage = "url('assets/3-0.0-1.0-0.1-67-0-1-10-5-8-0-0.png')";
+      break;
+      case 5:
+      document.body.style.backgroundColor = "#f3f3f3";
+      document.body.style.backgroundImage = "url('assets/3-0.0-1.0-0.15-100-0-2-11-25-22-18-5.png')";                break;
+      case 6:
+      document.body.style.backgroundColor = "#f3f3f3";
+      document.body.style.backgroundImage = "url('assets/4-0.0-0.0-0.0-71-0-2-17-7-38-52-3-16.png')";
+      break;
+      case 7:
+      document.body.style.backgroundColor = "#f3f3f3";
+      document.body.style.backgroundImage = "url('assets/4-0.0-0.0-0.0-116-1-1-5-15-39-35-3.png')";
+      break;
+      case 8:
+      document.body.style.backgroundColor = "#f3f3f3";
+      document.body.style.backgroundImage = "url('assets/4-0.0-0.0-0.0-131-1-0-2-1-16-0-3.png')";
+      break;
+      case 9:
+      document.body.style.backgroundColor = "#f3f3f3";
+      document.body.style.backgroundImage = "url('assets/5-1.0-1.0-1.0-61-0-4-21-6-42-0-1-8.png')";
+      break;
+      case 10:
+        document.body.style.backgroundColor = "#f3f3f3";
+                        document.body.style.backgroundImage = "url('assets/5-1.0-1.0-1.0-75-0-4-10-22-28-43-4-3.png')";
+                          break;
+                          case 11:
+                          document.body.style.backgroundColor = "#f3f3f3";
+                          document.body.style.backgroundImage = "url('assets/5-1.0-1.0-1.0-142-0-4-10-3-46-0-0.png')";
+                            break;
+                            case 12:
+                            document.body.style.backgroundColor = "#f3f3f3";
+                            document.body.style.backgroundImage = "url('assets/6-0.0-0.45-1.0-78-0-3-19-7-16-27-0.png')";
+                              break;
+                              case 13:
+                              document.body.style.backgroundColor = "#f3f3f3";
+                              document.body.style.backgroundImage = "url('assets/6-0.0-0.55-1.0-97-3-2-8-13-7-24-6.png')";
+                                break;
+                                case 14:
+                                document.body.style.backgroundColor = "#f3f3f3";
+                                document.body.style.backgroundImage = "url('assets/6-0.0-1.0-0.85-141-1-0-22-10-27-32-5.png')";
+                                  break;
+                                  case 15:
+                                  document.body.style.backgroundColor = "#f3f3f3";
+                                  document.body.style.backgroundImage = "url('assets/6-0.35-0.0-1.0-67-0-3-21-7-10-0-1.png')";
+                                    break;
+                                    case 16:
+                                    document.body.style.backgroundColor = "#f3f3f3";
+                                    document.body.style.backgroundImage = "url('assets/6-1.0-0.0-0.0-72-1-4-19-17-9-0-4.png')";
+                                      break;
+                                      case 17:
+                                      document.body.style.backgroundColor = "#f3f3f3";
+                                      document.body.style.backgroundImage = "url('assets/6-1.0-0.0-0.6-91-0-3-7-19-28-0-4.png')";
+                                        break;
+                                        case 18:
+                                        document.body.style.backgroundColor = "#f3f3f3";
+                                        document.body.style.backgroundImage = "url('assets/7-0.0-0.65-1.0-141-2-4-23-19-20-45-3.png')";
+                                          break;
+                                          case 19:
+                                          document.body.style.backgroundColor = "#f3f3f3";
+                                          document.body.style.backgroundImage = "url('assets/7-0.0-1.0-0.45-131-0-1-7-23-52-0-1-9.png')";
+                                            break;
+                                            case 20:
+                                            document.body.style.backgroundColor = "#f3f3f3";
+                                            document.body.style.backgroundImage = "url('assets/7-0.35-0.0-1.0-90-0-4-14-13-24-50-3.png')";
+                                              break;
+                                              case 21:
+                                              document.body.style.backgroundColor = "#f3f3f3";
+                                              document.body.style.backgroundImage = "url('assets/7-0.55-1.0-0.0-127-1-2-7-26-21-36-0.png')";
+                                                break;
+                                                case 22:
+                                                document.body.style.backgroundColor = "#f3f3f3";
+                                                document.body.style.backgroundImage = "url('assets/7-0.75-0.0-1.0-80-0-0-10-13-1-30-0.png')";
+                                                  break;
+                                                  case 23:
+                                                  document.body.style.backgroundColor = "#f3f3f3";
+                                                  document.body.style.backgroundImage = "url('assets/7-1.0-0.0-0.15-97-1-2-15-22-21-0-4.png')";
+                                                    break;
+                                                    case 24:
+                                                    document.body.style.backgroundColor = "#f3f3f3";
+                                                    document.body.style.backgroundImage = "url('assets/7-1.0-0.8-0.0-113-0-4-3-8-25-37-5-6.png')";
+                                                      break;
+                                                      case 25:
+                                                      document.body.style.backgroundColor = "#f3f3f3";
+                                                      document.body.style.backgroundImage = "url('assets/7-1.0-0.65-0.0-99-0-0-9-17-61-12-4.png')";
+                                                        break;
+                                                        case 26:
+                                                        document.body.style.backgroundColor = "#f3f3f3";
+                                                        document.body.style.backgroundImage = "url('assets/8-0.1-1.0-0.0-122-1-2-5-7-44-54-4.png')";
+                                                          break;
+                                                          case 27:
+                                                          document.body.style.backgroundColor = "#f3f3f3";
+                                                          document.body.style.backgroundImage = "url('')";
+                                                            break;
+                                                            case 28:
+                                                            document.body.style.backgroundColor = "#f3f3f3";
+                                                            document.body.style.backgroundImage = "url('assets/8-0.25-0.0-1.0-95-0-0-9-22-17-51-5.png')";
+                                                              break;
+                                                              case 29:
+                                                              document.body.style.backgroundColor = "#f3f3f3";
+                                                              document.body.style.backgroundImage = "url('assets/8-1.0-0.0-0.85-70-1-4-5-14-48-0-0-16.png')";
+                                                                break;                                                              case 30:
+                                                              document.body.style.backgroundColor = "#f3f3f3";
+                                                                document.body.style.backgroundImage = "url('assets/8-1.0-0.25-0.0-75-0-0-12-7-45-35-6.png')";
+                                                      break;
+                                                              case 31:
+                                                                document.body.style.backgroundColor = "#f3f3f3";
+                                                            document.body.style.backgroundImage = "url('assets/9-0.8-0.8-0.8-70-0-3-4-6-23-12-4.png')";
+                                                                  break;
+                                                                    case 32:
+                                                              document.body.style.backgroundColor = "#f3f3f3";
+                                                                  document.body.style.backgroundImage = "url('assets/9-0.8-0.8-0.8-102-0-3-13-28-61-0-0.png')";
+                                                                    break;
+                                                                    case 33:
+                                                                  document.body.style.backgroundColor = "#f3f3f3";
+                                                                document.body.style.backgroundImage = "url('assets/10-0.0-1.0-0.45-114-4-1-10-22-2-0-3.png')";
+                                                                        break;
+                                                                  case 34:
+                                                                    document.body.style.backgroundColor = "#f3f3f3";
+                                                                        document.body.style.backgroundImage = "url('assets/10-0.0-1.0-0.65-116-1-3-7-19-56-0-6-17.png')";
+                                                                          break;
+                                                                    case 35:
+                                                                          document.body.style.backgroundColor = "#f3f3f3";
+                                                                          document.body.style.backgroundImage = "url('assets/10-0.2-1.0-0.0-91-2-0-15-15-39-0-3-4.png')";
+                                                                      break;
+                                                                     case 36:
+                                                                            document.body.style.backgroundColor = "#f3f3f3";
+                                                                            document.body.style.backgroundImage = "url('assets/10-0.5-0.0-1.0-64-0-0-13-22-51-0-4-2.png')";
+                                                                            break;                                                               break;
+    default:
+      break;
+  }
+var num2 = getRandomInt(0, 10);
+switch (num2) {
+  case 0:
+  document.body.style.backgroundColor = "#ff0000";
+    break;
+    case 1:
+    document.body.style.backgroundColor = "#ff9000";
+      break;
+      case 2:
+      document.body.style.backgroundColor = "#fff200";
+        break;
+        case 3:
+        document.body.style.backgroundColor = "#94ff00";
+          break;
+          case 4:
+          document.body.style.backgroundColor = "#00ff1d";
+            break;
+            case 5:
+            document.body.style.backgroundColor = "#00ff99";
+              break;
+              case 6:
+              document.body.style.backgroundColor = "#00f6ff";
+                break;
+                case 7:
+                document.body.style.backgroundColor = "#000cff";
+                  break;
+                  case 8:
+                  document.body.style.backgroundColor = "#bb00ff";
+                    break;
+                    case 9:
+                    document.body.style.backgroundColor = "#ff00bf";
+                      break;
+                      case 10:
+                      document.body.style.backgroundColor = "#ffffff";
+                        break;
+  default:
+  break;
+}
+
+setTimeout(bg1,10000);
+}
+/*  if(typeof(Storage) !== "undefined") {
+      if (localStorage.clickcount) {
+          localStorage.clickcount = num;
+      } else {
+          localStorage.clickcount = num;
+      }
+  }*/
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
